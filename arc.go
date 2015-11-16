92d235fd63d2b4f4f6e25b92af0e43b993f7d379
John Gregory <jgregory@companieshouse.gov.uk>
2015-11-16 15:44:10 +0000

    Add some notes

--- arc.go
+++ arc.go
@@ -31,7 +31,46 @@ func main() {
 	log.Println("Initial guess is", guess)
 	log.Println("Precision is", precision)
 
-	calculate(lower, upper, precision, space, guess, 1)
+	// Ok, that's the set up stuff out of the way. Now to get on with actually
+	// calculating some things!
+
+	/* The theory:
+		   Given that, in a uniform tube (top and bottom circumference the same), there
+		   is no curve. Unrolled, this forms a rectangle.
+		   To increase the size of the top circumference, we increase the length of the
+		   top edge of the rectangle.
+		   As we do so, the two sides bow outwards proportionally (assuming that the lengthing
+		   begins at the midpoint and is applied uniformly in both directions). This leaves
+		   us with a parallelogram.
+		   Attmpting to re-roll this parallelogram (edge flat to edge) does not yield
+		   the correct result and gives us a skewed frustrum (i.e. cone with the top cut off).
+		   To achieve a smooth roll with flat top and bottom, a curve needs to be applied to
+		   the top and bottom edges.
+		   So how to determine that curve? If it's too shallow it's no better than the
+		   flat edge; if too deep it'll skew the other way.
+		   The theory runs that the correct curve is desribed by the circumference of
+		   a circle (i.e. the curve is uniform along it's length and, given the right
+		   size of circle, will fit perfectly on the circumference). Therefore we need
+		   to find the correct size of circle. The theory follows that, given the two
+		   curve lengths we want, at the particular distance from each other we require,
+		   they will only exist at a single radius (for the inner, and radius+distance for
+		   the outer) of the circle.
+	     Therefore the method is determine that radius and the angle that defines
+	     the sector.
+	     How do we do it? Frankly: guess. Then refine.
+	     - Make a guess about the radius that describes the lower arc.
+	     - Using the calculated angle that gives is the arc at that radius, add the
+	       spacer distance and work out what upper arc that would give us.
+	     - If it's too big or small (not within a defined margin of acceptance) then
+	       refine our guess and try again.
+	*/
+
+	radius, theta := calculateRadiusAndTheta(lower, upper, precision, space, guess, 1)
+
+	log.Printf("Upper arc %v within precision: base radius is %v\n", radius, guess)
+	log.Printf("Sagitta is [%0.2f]", makeSagitta(theta, guess))
+	log.Printf("Theta is [%0.2f]", theta)
+
 	os.Exit(0)
 }
 
@@ -48,39 +87,39 @@ func makeArc(theta float64, radius float64) float64 {
 // The sagitta is the tangential distance between the center of the chord
 // of the sector and the outline of the circle
 func makeSagitta(theta float64, radius float64) float64 {
-	// return radius * (1 - math.Cos(theta/2))
+	// To determine the sagitta we need to know half the length of the chord
+	l := radius * math.Sin(theta/2)
 
-	//
-	log.Println("RADIUS ", radius)
-	log.Println("PART ", 1-radius*math.Cos(theta/2)*-1)
-	//
-	// return radius - (1 - radius*math.Cos(theta/2))
-	return radius - (radius * math.Cos(theta/2))
+	log.Println("Half length is ", l)
+
+	return radius * (1 - math.Cos(theta/2))
+
+	// //
+	// log.Println("RADIUS ", radius)
+	// log.Println("PART ", 1-radius*math.Cos(theta/2)*-1)
+	// //
+	// // return radius - (1 - radius*math.Cos(theta/2))
+	// return radius - (radius * math.Cos(theta/2))
 }
 
-func calculate(l float64, u float64, m float64, space float64, guess float64, it int) {
+func calculateRadiusAndTheta(l float64, u float64, m float64, space float64, guess float64, it int) (radius float64, theta float64) {
 	// Work out the theta on the lower arc given the guess
-	theta := makeTheta(l, guess)
+	theta = makeTheta(l, guess)
 	// Now calculate what the upper arc *would* be given the calculated theta
-	calculatedUpperArc := makeArc(theta, guess+space)
+	radius = makeArc(theta, guess+space)
 
 	// Test whether the calculated arc is within the precision we have accepted.
 	// If it is then we take it and return out.
-	if calculatedUpperArc >= u-m && calculatedUpperArc <= u+m {
-		log.Printf("Upper arc %v within precision: base radius is %v\n", calculatedUpperArc, guess)
-		log.Printf("Sagitta is [%0.2f]", makeSagitta(theta, guess))
-		log.Printf("Theta is [%0.2f]", theta)
+	if radius >= u-m && radius <= u+m {
 		log.Printf("Calculated in [%v] recursions", it)
 		return
 	}
 
 	// Recurse!
-	if calculatedUpperArc > u {
-		log.Printf("Too big [%0.2f], guess again with [%v]", calculatedUpperArc, guess*2)
-		calculate(l, u, m, space, math.Pow(guess, 2), it+1)
-	} else {
-		log.Printf("Too small [%0.2f], guess again with [%v]", calculatedUpperArc, guess/2)
-		calculate(l, u, m, space, guess/2, it+1)
+	if radius > u {
+		log.Printf("Too big [%0.2f], guess again with [%v]", radius, guess*2)
+		return calculateRadiusAndTheta(l, u, m, space, math.Pow(guess, 2), it+1)
 	}
-
+	log.Printf("Too small [%0.2f], guess again with [%v]", radius, guess/2)
+	return calculateRadiusAndTheta(l, u, m, space, guess/2, it+1)
 }
